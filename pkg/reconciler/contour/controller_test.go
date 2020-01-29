f9acaed52e774077adf2f8e78deaf60afb902007
Matt Moore <mattmoor@vmware.com>
2020-01-28 21:10:10 -0800

    Add the controller and configs.
    This change has a couple parts:
    1. Adds the controller constructor,
    2. Adds the controller `main.go`,
    3. Adds the `net-contour` configuration,
    4. Adds `config/contour` to bootstrap a working Contour installation,
    5. Update DEVELOPMENT.md with how to get set up.

---
+++ pkg/reconciler/contour/controller_test.go
@@ -0,0 +1,56 @@
+/*
+Copyright 2020 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package contour
+
+import (
+	"testing"
+
+	_ "knative.dev/net-contour/pkg/client/injection/informers/projectcontour/v1/httpproxy/fake"
+	_ "knative.dev/pkg/client/injection/kube/informers/core/v1/endpoints/fake"
+	_ "knative.dev/pkg/client/injection/kube/informers/core/v1/pod/fake"
+	_ "knative.dev/pkg/client/injection/kube/informers/core/v1/service/fake"
+	_ "knative.dev/serving/pkg/client/injection/informers/networking/v1alpha1/ingress/fake"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"knative.dev/net-contour/pkg/reconciler/contour/config"
+	"knative.dev/pkg/configmap"
+	"knative.dev/pkg/system"
+	"knative.dev/serving/pkg/network"
+
+	. "knative.dev/pkg/reconciler/testing"
+)
+
+func TestNew(t *testing.T) {
+	ctx, _ := SetupFakeContext(t)
+
+	c := NewController(ctx, configmap.NewStaticWatcher(&corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: system.Namespace(),
+			Name:      config.ContourConfigName,
+		},
+	}, &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: system.Namespace(),
+			Name:      network.ConfigName,
+		},
+	}))
+
+	if c == nil {
+		t.Fatal("Expected NewController to return a non-nil value")
+	}
+}
--- vendor/k8s.io/api/admission/v1beta1/doc.go
+++ vendor/k8s.io/api/admission/v1beta1/doc.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 The Knative Authors
+Copyright 2017 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,18 +14,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package main
+// +k8s:deepcopy-gen=package
+// +k8s:protobuf-gen=package
+// +k8s:openapi-gen=false
 
-import (
-	// The set of controllers this controller process runs.
-	"knative.dev/net-contour/pkg/reconciler/contour"
+// +groupName=admission.k8s.io
 
-	// This defines the shared main for injected controllers.
-	"knative.dev/pkg/injection/sharedmain"
-)
-
-func main() {
-	sharedmain.Main("controller",
-		contour.NewController,
-	)
-}
+package v1beta1 // import "k8s.io/api/admission/v1beta1"
