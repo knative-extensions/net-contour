b04056a846a40ffe5ffc4a42cd720f41a50023c1
Matt Moore <mattmoor@vmware.com>
2020-08-16 14:32:11 -0700

    Add log decoration to give more context at the log site.

--- pkg/reconciler/contour/contour.go
+++ pkg/reconciler/contour/contour.go
@@ -73,6 +73,10 @@ var _ ingressreconciler.Interface = (*Reconciler)(nil)
 // ReconcileKind reconciles ingress resource.
 func (r *Reconciler) ReconcileKind(ctx context.Context, ing *v1alpha1.Ingress) reconciler.Event {
 	logger := logging.FromContext(ctx)
+	logger = logger.With(
+		zap.Int64("generation", ing.Generation),
+		zap.String("resource-version", ing.ResourceVersion),
+	)
 
 	// Track whether there is an endpoint probe kingress to clean up.
 	haveEndpointProbe := false
@@ -82,16 +86,16 @@ func (r *Reconciler) ReconcileKind(ctx context.Context, ing *v1alpha1.Ingress) r
 		// kingress. Stop recursing when we see our annotation and proceed to
 		// HTTP Proxy and probing.
 		logger.Debug("Avoiding endpoint probe recursion.")
-	} else
-	// See if we have any HTTPProxy resources for this generation.
-	// We only create HTTPProxy resources once we have successfully probed
-	// a generation's endpoints.
-	if elts, err := r.contourLister.HTTPProxies(ing.Namespace).List(labels.Set(map[string]string{
-		resources.ParentKey:     ing.Name,
-		resources.GenerationKey: fmt.Sprintf("%d", ing.Generation),
-	}).AsSelector()); err != nil {
+	} else if currentGeneration, err := r.contourLister.HTTPProxies(ing.Namespace).List(
+		// See if we have any HTTPProxy resources for this generation.
+		// We only create HTTPProxy resources once we have successfully probed
+		// a generation's endpoints.
+		labels.Set(map[string]string{
+			resources.ParentKey:     ing.Name,
+			resources.GenerationKey: fmt.Sprintf("%d", ing.Generation),
+		}).AsSelector()); err != nil {
 		return err
-	} else if len(elts) == 0 {
+	} else if len(currentGeneration) == 0 {
 		// There are no HTTPProxy resources with the current generation.
 		// Reconcile an endpoint probe child kingress to ensure the Contour
 		// gateways have the endpoints for our generation's services.
@@ -106,30 +110,31 @@ func (r *Reconciler) ReconcileKind(ctx context.Context, ing *v1alpha1.Ingress) r
 		if err != nil {
 			return err
 		}
-		elts, err := r.contourLister.HTTPProxies(ing.Namespace).List(selector)
+		oldGeneration, err := r.contourLister.HTTPProxies(ing.Namespace).List(selector)
 		if err != nil {
 			return err
 		}
-		logger.Debugf("Found %d HTTP Proxies from older generations.", len(elts))
+		logger.Debugf("Found %d HTTP Proxies from older generations.", len(oldGeneration))
 
-		desiredChIng := resources.MakeEndpointProbeIngress(ctx, ing, elts)
+		desiredChIng := resources.MakeEndpointProbeIngress(ctx, ing, oldGeneration)
 		actualChIng, err := r.ingressLister.Ingresses(desiredChIng.Namespace).Get(desiredChIng.Name)
 		if apierrs.IsNotFound(err) { // Create it.
 			actualChIng, err = r.ingressClient.NetworkingV1alpha1().Ingresses(desiredChIng.Namespace).Create(desiredChIng)
 			if err != nil {
 				return err
 			}
-			logger.Debugf("Created endpoint probe: %v", actualChIng)
+			logger.Debugf("Created endpoint probe: %#v", actualChIng.Spec)
 		} else if err != nil {
 			return err
 		} else if !equality.Semantic.DeepEqual(actualChIng.Spec, desiredChIng.Spec) { // Reconcile it.
-			actualChIng = actualChIng.DeepCopy()
+			original := actualChIng
+			actualChIng = original.DeepCopy()
 			actualChIng.Spec = desiredChIng.Spec
 			actualChIng, err = r.ingressClient.NetworkingV1alpha1().Ingresses(actualChIng.Namespace).Update(actualChIng)
 			if err != nil {
 				return err
 			}
-			if diff, err := kmp.SafeDiff(desiredChIng.Spec, actualChIng.Spec); err == nil {
+			if diff, err := kmp.SafeDiff(actualChIng.Spec, original.Spec); err == nil {
 				logger.Debugf("Updated endpoint probe: %s", diff)
 			} else {
 				logger.Warnw("Error diffing endpoint probes", zap.Error(err))
@@ -143,16 +148,17 @@ func (r *Reconciler) ReconcileKind(ctx context.Context, ing *v1alpha1.Ingress) r
 
 		// The endpoints ingress is ready, we are good to go!
 		haveEndpointProbe = true
-		logger.Debugf("We have an endpoint probe: %#v.", actualChIng)
+		logger.Debugf("We have an endpoint probe: %#v.", actualChIng.Spec)
 	} else {
 		ing, err := r.ingressLister.Ingresses(ing.Namespace).Get(names.EndpointProbeIngress(ing))
-		haveEndpointProbe = !apierrs.IsNotFound(err)
+		haveEndpointProbe = (err == nil || !apierrs.IsNotFound(err))
 		if haveEndpointProbe {
-			logger.Debugf("We have an endpoint probe: %#v.", ing)
+			logger.Debugf("We have an endpoint probe. ready: %v, spec: %#v", ing.IsReady(), ing.Spec)
 		} else {
 			logger.Debug("We do not have an endpoint probe.")
 		}
 	}
+	logger = logger.With(zap.Bool("have-endpoint-probe", haveEndpointProbe))
 
 	info := resources.ServiceNames(ctx, ing)
 	serviceNames := make(sets.String, len(info))
@@ -160,6 +166,7 @@ func (r *Reconciler) ReconcileKind(ctx context.Context, ing *v1alpha1.Ingress) r
 	for name := range info {
 		serviceNames.Insert(name)
 	}
+	logger = logger.With(zap.Strings("services", serviceNames.List()))
 
 	// Establish the protocol for each Service, and ensure that their Endpoints are
 	// populated with Ready addresses before we reprogram Contour.
@@ -190,11 +197,11 @@ func (r *Reconciler) ReconcileKind(ctx context.Context, ing *v1alpha1.Ingress) r
 			resources.DomainHashKey: proxy.Labels[resources.DomainHashKey],
 			resources.ClassKey:      proxy.Labels[resources.ClassKey],
 		}).AsSelector()
-		elts, err := r.contourLister.HTTPProxies(ing.Namespace).List(selector)
+		matches, err := r.contourLister.HTTPProxies(ing.Namespace).List(selector)
 		if err != nil {
 			return err
 		}
-		if len(elts) == 0 {
+		if len(matches) == 0 {
 			proxy, err := r.contourClient.ProjectcontourV1().HTTPProxies(proxy.Namespace).Create(proxy)
 			if err != nil {
 				return err
@@ -202,37 +209,40 @@ func (r *Reconciler) ReconcileKind(ctx context.Context, ing *v1alpha1.Ingress) r
 			logger.Debugf("Created http proxy: %#v", proxy)
 			continue
 		}
-		update := elts[0].DeepCopy()
+		update := matches[0].DeepCopy()
 		update.Annotations = proxy.Annotations
 		update.Labels = proxy.Labels
 		update.Spec = proxy.Spec
-		if equality.Semantic.DeepEqual(elts[0], update) {
+		if equality.Semantic.DeepEqual(matches[0], update) {
 			// Avoid updates that don't change anything.
 			continue
 		}
 		if _, err = r.contourClient.ProjectcontourV1().HTTPProxies(proxy.Namespace).Update(update); err != nil {
 			return err
 		}
-		if diff, err := kmp.SafeDiff(update, elts[0]); err == nil {
+		if diff, err := kmp.SafeDiff(update, matches[0]); err == nil {
 			logger.Debugf("Updated http proxy: %s", diff)
 		} else {
 			logger.Warnw("Error diffing http proxy", zap.Error(err))
 		}
 	}
 
-	// Before deleting old programming, check our cached to see whether there is anything to clean up.
+	// Before deleting old programming, check our cache to see whether there is anything to clean up.
 	if selector, err := labels.Parse(fmt.Sprintf("%s=%s,%s!=%d",
 		resources.ParentKey, ing.Name,
 		resources.GenerationKey, ing.Generation)); err != nil {
 		return err
-	} else if elts, err := r.contourLister.HTTPProxies(ing.Namespace).List(selector); err != nil {
+	} else if leftovers, err := r.contourLister.HTTPProxies(ing.Namespace).List(selector); err != nil {
 		return err
-	} else if len(elts) != 0 {
+	} else if len(leftovers) != 0 {
+		logger.Debugf("Deleting %d older http proxies.", len(leftovers))
+		for _, leftover := range leftovers {
+			logger.Debugf("Leftover: %#v.", leftover)
+		}
 		if err := r.contourClient.ProjectcontourV1().HTTPProxies(ing.Namespace).DeleteCollection(
 			&metav1.DeleteOptions{}, metav1.ListOptions{LabelSelector: selector.String()}); err != nil {
 			return err
 		}
-		logger.Debugf("Deleted %d older http proxies.", len(elts))
 	}
 	ing.Status.MarkNetworkConfigured()
 
