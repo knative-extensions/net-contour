cb28167c2ada0d3f230aeb4d993fe8e6cdd9172e
Matt Moore <mattmoor@vmware.com>
2020-08-23 12:29:13 -0700

    Configure default retries matching Istio

--- pkg/reconciler/contour/resources/httpproxy.go
+++ pkg/reconciler/contour/resources/httpproxy.go
@@ -113,15 +113,27 @@ func MakeHTTPProxies(ctx context.Context, ing *v1alpha1.Ingress, serviceToProtoc
 				}
 			}
 
-			var retry *v1.RetryPolicy
+			// By default retry on connection problems twice.
+			// This matches the default behavior of Istio:
+			// https://istio.io/latest/docs/concepts/traffic-management/#retries
+			retry := &v1.RetryPolicy{
+				NumRetries: 2,
+				RetryOn: []v1.RetryOn{
+					"connect-failure",
+					"refused-stream",
+					"cancelled",
+					"resource-exhausted",
+				},
+			}
 			if path.Retries != nil && path.Retries.Attempts > 0 {
-				retry = &v1.RetryPolicy{
-					NumRetries: int64(path.Retries.Attempts),
-				}
+				retry.NumRetries = int64(path.Retries.Attempts)
+
+				// When retries is specified explicitly, then we retry some http-level failures as well.
+				retry.RetryOn = append(retry.RetryOn, "retriable-status-codes", "5xx")
+
 				if path.Retries.PerTryTimeout != nil {
 					retry.PerTryTimeout = path.Retries.PerTryTimeout.Duration.String()
 				}
-
 			}
 
 			preSplitHeaders := &v1.HeadersPolicy{
