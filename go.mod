11285aa2e516424512e348b72340053810a14e3c
Matt Moore <mattmoor@vmware.com>
2020-09-10 20:11:18 -0700

    K8s 1.18 libs

--- go.mod
+++ go.mod
@@ -8,17 +8,20 @@ require (
 	github.com/projectcontour/contour v1.8.1
 	go.uber.org/zap v1.15.0
 	gopkg.in/yaml.v2 v2.3.0
-	k8s.io/api v0.18.7-rc.0
-	k8s.io/apimachinery v0.18.7-rc.0
+	k8s.io/api v0.18.8
+	k8s.io/apimachinery v0.18.8
 	k8s.io/client-go v11.0.1-0.20190805182717-6502b5e7b1b5+incompatible
-	knative.dev/networking v0.0.0-20200910034251-6c4bda194884
-	knative.dev/pkg v0.0.0-20200910010051-a79a813ce123
-	knative.dev/test-infra v0.0.0-20200909211651-72eb6ae3c773
+	knative.dev/networking v0.0.0-20200911160100-731bfc03416d
+	knative.dev/pkg v0.0.0-20200911145400-2d4efecc6bc1
+	knative.dev/test-infra v0.0.0-20200910231400-cfba2288403d
 )
 
 replace (
-	k8s.io/api => k8s.io/api v0.17.6
-	k8s.io/apimachinery => k8s.io/apimachinery v0.17.6
-	k8s.io/client-go => k8s.io/client-go v0.17.6
-	k8s.io/code-generator => k8s.io/code-generator v0.17.6
+	k8s.io/api => k8s.io/api v0.18.8
+	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.18.8
+	k8s.io/apimachinery => k8s.io/apimachinery v0.18.8
+	k8s.io/apiserver => k8s.io/apiserver v0.18.8
+	k8s.io/client-go => k8s.io/client-go v0.18.8
+	k8s.io/code-generator => k8s.io/code-generator v0.18.8
+	k8s.io/kube-openapi => k8s.io/kube-openapi v0.0.0-20200410145947-61e04a5be9a6
 )
--- pkg/reconciler/contour/contour.go
+++ pkg/reconciler/contour/contour.go
@@ -119,7 +119,7 @@ func (r *Reconciler) ReconcileKind(ctx context.Context, ing *v1alpha1.Ingress) r
 		desiredChIng := resources.MakeEndpointProbeIngress(ctx, ing, oldGeneration)
 		actualChIng, err := r.ingressLister.Ingresses(desiredChIng.Namespace).Get(desiredChIng.Name)
 		if apierrs.IsNotFound(err) { // Create it.
-			actualChIng, err = r.ingressClient.NetworkingV1alpha1().Ingresses(desiredChIng.Namespace).Create(desiredChIng)
+			actualChIng, err = r.ingressClient.NetworkingV1alpha1().Ingresses(desiredChIng.Namespace).Create(ctx, desiredChIng, metav1.CreateOptions{})
 			if err != nil {
 				return err
 			}
@@ -130,7 +130,7 @@ func (r *Reconciler) ReconcileKind(ctx context.Context, ing *v1alpha1.Ingress) r
 			original := actualChIng
 			actualChIng = original.DeepCopy()
 			actualChIng.Spec = desiredChIng.Spec
-			actualChIng, err = r.ingressClient.NetworkingV1alpha1().Ingresses(actualChIng.Namespace).Update(actualChIng)
+			actualChIng, err = r.ingressClient.NetworkingV1alpha1().Ingresses(actualChIng.Namespace).Update(ctx, actualChIng, metav1.UpdateOptions{})
 			if err != nil {
 				return err
 			}
@@ -204,7 +204,7 @@ func (r *Reconciler) ReconcileKind(ctx context.Context, ing *v1alpha1.Ingress) r
 			return err
 		}
 		if len(matches) == 0 {
-			proxy, err := r.contourClient.ProjectcontourV1().HTTPProxies(proxy.Namespace).Create(proxy)
+			proxy, err := r.contourClient.ProjectcontourV1().HTTPProxies(proxy.Namespace).Create(ctx, proxy, metav1.CreateOptions{})
 			if err != nil {
 				return err
 			}
@@ -219,7 +219,7 @@ func (r *Reconciler) ReconcileKind(ctx context.Context, ing *v1alpha1.Ingress) r
 			// Avoid updates that don't change anything.
 			continue
 		}
-		if _, err = r.contourClient.ProjectcontourV1().HTTPProxies(proxy.Namespace).Update(update); err != nil {
+		if _, err = r.contourClient.ProjectcontourV1().HTTPProxies(proxy.Namespace).Update(ctx, update, metav1.UpdateOptions{}); err != nil {
 			return err
 		}
 		if diff, err := kmp.SafeDiff(update, matches[0]); err == nil {
@@ -242,7 +242,7 @@ func (r *Reconciler) ReconcileKind(ctx context.Context, ing *v1alpha1.Ingress) r
 			logger.Debugf("Leftover: %#v.", leftover)
 		}
 		if err := r.contourClient.ProjectcontourV1().HTTPProxies(ing.Namespace).DeleteCollection(
-			&metav1.DeleteOptions{}, metav1.ListOptions{LabelSelector: selector.String()}); err != nil {
+			ctx, metav1.DeleteOptions{}, metav1.ListOptions{LabelSelector: selector.String()}); err != nil {
 			return err
 		}
 	}
@@ -283,7 +283,7 @@ func (r *Reconciler) ReconcileKind(ctx context.Context, ing *v1alpha1.Ingress) r
 		if ing.IsReady() {
 			// Delete the endpoints probe once we have reached a steady state.
 			if err := r.ingressClient.NetworkingV1alpha1().Ingresses(ing.Namespace).Delete(
-				names.EndpointProbeIngress(ing), &metav1.DeleteOptions{}); err != nil {
+				ctx, names.EndpointProbeIngress(ing), metav1.DeleteOptions{}); err != nil {
 				return err
 			}
 			logger.Debug("Deleted endpoint probe.")
