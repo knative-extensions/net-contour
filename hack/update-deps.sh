c3c22bb934a9e62dc024567c99e3ecf4139d25d2
dprotaso <dprotaso@gmail.com>
2022-07-11 10:11:36 -0400

    bump contour to v1.21.1

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -20,8 +20,8 @@ set -o pipefail
 
 source $(dirname "$0")/../vendor/knative.dev/hack/library.sh
 
-CONTOUR_VERSION="v1.20.1" # This is for controlling which version of contour we want to use.
-CONTOUR_OPERATOR_VERSION="v1.20.1" # This is for controlling which version of contour-operator we want to use.
+CONTOUR_VERSION="v1.21.1" # This is for controlling which version of contour we want to use.
+CONTOUR_OPERATOR_VERSION="v1.21.1" # This is for controlling which version of contour-operator we want to use.
 
 CLUSTER_ROLE_NAME=knative-contour
 
