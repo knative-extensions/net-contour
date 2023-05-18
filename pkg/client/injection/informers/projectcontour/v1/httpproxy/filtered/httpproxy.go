06ca77f97d537ac572d0b115782d26b36aaa0851
knative-automation <automation@knative.team>
2023-05-18 13:28:12 -0400

    upgrade to latest dependencies (#910)
    bumping knative.dev/pkg db8a353...dfb4bf0:%0A  > dfb4bf0 Drop dynamic wrapper injection code generation (# 2742)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- pkg/client/injection/informers/projectcontour/v1/httpproxy/filtered/httpproxy.go
+++ pkg/client/injection/informers/projectcontour/v1/httpproxy/filtered/httpproxy.go
@@ -21,15 +21,8 @@ package filtered
 import (
 	context "context"
 
-	apisprojectcontourv1 "github.com/projectcontour/contour/apis/projectcontour/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	labels "k8s.io/apimachinery/pkg/labels"
-	cache "k8s.io/client-go/tools/cache"
-	versioned "knative.dev/net-contour/pkg/client/clientset/versioned"
 	v1 "knative.dev/net-contour/pkg/client/informers/externalversions/projectcontour/v1"
-	client "knative.dev/net-contour/pkg/client/injection/client"
 	filtered "knative.dev/net-contour/pkg/client/injection/informers/factory/filtered"
-	projectcontourv1 "knative.dev/net-contour/pkg/client/listers/projectcontour/v1"
 	controller "knative.dev/pkg/controller"
 	injection "knative.dev/pkg/injection"
 	logging "knative.dev/pkg/logging"
@@ -37,7 +30,6 @@ import (
 
 func init() {
 	injection.Default.RegisterFilteredInformers(withInformer)
-	injection.Dynamic.RegisterDynamicInformer(withDynamicInformer)
 }
 
 // Key is used for associating the Informer inside the context.Context.
@@ -62,20 +54,6 @@ func withInformer(ctx context.Context) (context.Context, []controller.Informer) 
 	return ctx, infs
 }
 
-func withDynamicInformer(ctx context.Context) context.Context {
-	untyped := ctx.Value(filtered.LabelKey{})
-	if untyped == nil {
-		logging.FromContext(ctx).Panic(
-			"Unable to fetch labelkey from context.")
-	}
-	labelSelectors := untyped.([]string)
-	for _, selector := range labelSelectors {
-		inf := &wrapper{client: client.Get(ctx), selector: selector}
-		ctx = context.WithValue(ctx, Key{Selector: selector}, inf)
-	}
-	return ctx
-}
-
 // Get extracts the typed informer from the context.
 func Get(ctx context.Context, selector string) v1.HTTPProxyInformer {
 	untyped := ctx.Value(Key{Selector: selector})
@@ -84,53 +62,4 @@ func Get(ctx context.Context, selector string) v1.HTTPProxyInformer {
 			"Unable to fetch knative.dev/net-contour/pkg/client/informers/externalversions/projectcontour/v1.HTTPProxyInformer with selector %s from context.", selector)
 	}
 	return untyped.(v1.HTTPProxyInformer)
-}
-
-type wrapper struct {
-	client versioned.Interface
-
-	namespace string
-
-	selector string
-}
-
-var _ v1.HTTPProxyInformer = (*wrapper)(nil)
-var _ projectcontourv1.HTTPProxyLister = (*wrapper)(nil)
-
-func (w *wrapper) Informer() cache.SharedIndexInformer {
-	return cache.NewSharedIndexInformer(nil, &apisprojectcontourv1.HTTPProxy{}, 0, nil)
-}
-
-func (w *wrapper) Lister() projectcontourv1.HTTPProxyLister {
-	return w
-}
-
-func (w *wrapper) HTTPProxies(namespace string) projectcontourv1.HTTPProxyNamespaceLister {
-	return &wrapper{client: w.client, namespace: namespace, selector: w.selector}
-}
-
-func (w *wrapper) List(selector labels.Selector) (ret []*apisprojectcontourv1.HTTPProxy, err error) {
-	reqs, err := labels.ParseToRequirements(w.selector)
-	if err != nil {
-		return nil, err
-	}
-	selector = selector.Add(reqs...)
-	lo, err := w.client.ProjectcontourV1().HTTPProxies(w.namespace).List(context.TODO(), metav1.ListOptions{
-		LabelSelector: selector.String(),
-		// TODO(mattmoor): Incorporate resourceVersion bounds based on staleness criteria.
-	})
-	if err != nil {
-		return nil, err
-	}
-	for idx := range lo.Items {
-		ret = append(ret, &lo.Items[idx])
-	}
-	return ret, nil
-}
-
-func (w *wrapper) Get(name string) (*apisprojectcontourv1.HTTPProxy, error) {
-	// TODO(mattmoor): Check that the fetched object matches the selector.
-	return w.client.ProjectcontourV1().HTTPProxies(w.namespace).Get(context.TODO(), name, metav1.GetOptions{
-		// TODO(mattmoor): Incorporate resourceVersion bounds based on staleness criteria.
-	})
 }
