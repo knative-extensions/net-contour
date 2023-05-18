0946b7f099f9cf113a94212d4d6ea872dd5b0f9b
Knative Automation <automation@knative.team>
2023-05-18 13:52:09 +0000

    upgrade to latest dependencies
    bumping knative.dev/pkg db8a353...dfb4bf0:%0A  > dfb4bf0 Drop dynamic wrapper injection code generation (# 2742)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- pkg/client/injection/informers/projectcontour/v1/httpproxy/httpproxy.go
+++ pkg/client/injection/informers/projectcontour/v1/httpproxy/httpproxy.go
@@ -21,15 +21,8 @@ package httpproxy
 import (
 	context "context"
 
-	apisprojectcontourv1 "github.com/projectcontour/contour/apis/projectcontour/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	labels "k8s.io/apimachinery/pkg/labels"
-	cache "k8s.io/client-go/tools/cache"
-	versioned "knative.dev/net-contour/pkg/client/clientset/versioned"
 	v1 "knative.dev/net-contour/pkg/client/informers/externalversions/projectcontour/v1"
-	client "knative.dev/net-contour/pkg/client/injection/client"
 	factory "knative.dev/net-contour/pkg/client/injection/informers/factory"
-	projectcontourv1 "knative.dev/net-contour/pkg/client/listers/projectcontour/v1"
 	controller "knative.dev/pkg/controller"
 	injection "knative.dev/pkg/injection"
 	logging "knative.dev/pkg/logging"
@@ -37,7 +30,6 @@ import (
 
 func init() {
 	injection.Default.RegisterInformer(withInformer)
-	injection.Dynamic.RegisterDynamicInformer(withDynamicInformer)
 }
 
 // Key is used for associating the Informer inside the context.Context.
@@ -49,11 +41,6 @@ func withInformer(ctx context.Context) (context.Context, controller.Informer) {
 	return context.WithValue(ctx, Key{}, inf), inf.Informer()
 }
 
-func withDynamicInformer(ctx context.Context) context.Context {
-	inf := &wrapper{client: client.Get(ctx), resourceVersion: injection.GetResourceVersion(ctx)}
-	return context.WithValue(ctx, Key{}, inf)
-}
-
 // Get extracts the typed informer from the context.
 func Get(ctx context.Context) v1.HTTPProxyInformer {
 	untyped := ctx.Value(Key{})
@@ -62,55 +49,4 @@ func Get(ctx context.Context) v1.HTTPProxyInformer {
 			"Unable to fetch knative.dev/net-contour/pkg/client/informers/externalversions/projectcontour/v1.HTTPProxyInformer from context.")
 	}
 	return untyped.(v1.HTTPProxyInformer)
-}
-
-type wrapper struct {
-	client versioned.Interface
-
-	namespace string
-
-	resourceVersion string
-}
-
-var _ v1.HTTPProxyInformer = (*wrapper)(nil)
-var _ projectcontourv1.HTTPProxyLister = (*wrapper)(nil)
-
-func (w *wrapper) Informer() cache.SharedIndexInformer {
-	return cache.NewSharedIndexInformer(nil, &apisprojectcontourv1.HTTPProxy{}, 0, nil)
-}
-
-func (w *wrapper) Lister() projectcontourv1.HTTPProxyLister {
-	return w
-}
-
-func (w *wrapper) HTTPProxies(namespace string) projectcontourv1.HTTPProxyNamespaceLister {
-	return &wrapper{client: w.client, namespace: namespace, resourceVersion: w.resourceVersion}
-}
-
-// SetResourceVersion allows consumers to adjust the minimum resourceVersion
-// used by the underlying client.  It is not accessible via the standard
-// lister interface, but can be accessed through a user-defined interface and
-// an implementation check e.g. rvs, ok := foo.(ResourceVersionSetter)
-func (w *wrapper) SetResourceVersion(resourceVersion string) {
-	w.resourceVersion = resourceVersion
-}
-
-func (w *wrapper) List(selector labels.Selector) (ret []*apisprojectcontourv1.HTTPProxy, err error) {
-	lo, err := w.client.ProjectcontourV1().HTTPProxies(w.namespace).List(context.TODO(), metav1.ListOptions{
-		LabelSelector:   selector.String(),
-		ResourceVersion: w.resourceVersion,
-	})
-	if err != nil {
-		return nil, err
-	}
-	for idx := range lo.Items {
-		ret = append(ret, &lo.Items[idx])
-	}
-	return ret, nil
-}
-
-func (w *wrapper) Get(name string) (*apisprojectcontourv1.HTTPProxy, error) {
-	return w.client.ProjectcontourV1().HTTPProxies(w.namespace).Get(context.TODO(), name, metav1.GetOptions{
-		ResourceVersion: w.resourceVersion,
-	})
 }
