7167871d4d05a9383ce96159c58dde515db77ba4
Izabela Gomes <igomes@vmware.com>
2024-04-09 13:58:40 -0400

    bump Contour version to v1.28.2 in update-deps script

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -20,7 +20,7 @@ set -o pipefail
 
 source $(dirname "$0")/../vendor/knative.dev/hack/library.sh
 
-CONTOUR_VERSION="v1.28.1" # This is for controlling which version of contour we want to use.
+CONTOUR_VERSION="v1.28.2" # This is for controlling which version of contour we want to use.
 CLUSTER_ROLE_NAME=knative-contour
 
 go get "github.com/projectcontour/contour@${CONTOUR_VERSION}"
